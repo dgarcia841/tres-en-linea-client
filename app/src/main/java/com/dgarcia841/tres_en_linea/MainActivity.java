fddfd74fd14944894e2289cd586770e90d581cdb
NiuWeb <101196831+dgarcia841@users.noreply.github.com>
2022-04-02 19:14:17 -0500

    Security config for local connections

--- app/src/main/java/com/dgarcia841/tres_en_linea/MainActivity.java
+++ app/src/main/java/com/dgarcia841/tres_en_linea/MainActivity.java
@@ -3,6 +3,10 @@ package com.dgarcia841.tres_en_linea;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView test = (ImageView) findViewById(R.id.iv_test);
+        Glide.with(this).load("http://192.168.0.192:3000").into(test);
     }
 }
