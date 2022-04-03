e2d6b427f0755888eb36bad418f9bb7b930850dd
NiuWeb <101196831+dgarcia841@users.noreply.github.com>
2022-04-02 20:23:19 -0500

    configured socket.io for server connection

--- app/src/main/java/com/dgarcia841/tres_en_linea/MainActivity.java
+++ app/src/main/java/com/dgarcia841/tres_en_linea/MainActivity.java
@@ -3,10 +3,17 @@ package com.dgarcia841.tres_en_linea;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+
+import java.net.URI;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +24,12 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView test = (ImageView) findViewById(R.id.iv_test);
         Glide.with(this).load("http://192.168.0.192:3000").into(test);
+
+
+        Socket socket = IO.socket(URI.create("http://192.168.0.192:4500"));
+        socket.on(Socket.EVENT_CONNECT, args -> {
+            Log.d("SOCKET", "connected!");
+        });
+        socket.connect();
     }
 }
