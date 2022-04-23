ad25b38163f92f2aa011dd4e18e3a1e3f13b61e6
NiuWeb <101196831+dgarcia841@users.noreply.github.com>
2022-04-23 17:11:35 -0500

    recoger datos del juego y enviarlos a una nueva actividad

--- app/src/main/java/com/dgarcia841/tres_en_linea/GameActivity.java
+++ app/src/main/java/com/dgarcia841/tres_en_linea/GameActivity.java
@@ -3,12 +3,35 @@ package com.dgarcia841.tres_en_linea;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
+    TextView tvGameid;
+    TextView tvRival;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        tvGameid = (TextView) findViewById(R.id.tv_gameid);
+        tvRival = (TextView) findViewById(R.id.tv_rival);
+
+        // Obtener datos del juego
+        Bundle data = getIntent().getExtras();
+
+        tvGameid.setText(getString(R.string.label_gameid) + " " + data.getString("gameid"));
+        tvRival.setText(getString(R.string.label_rival) + " " + data.getString("rivalname"));
+    }
+
+    /**
+     * Disconnect client on activity pause
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+        GameServer.get().getSocket().disconnect();
     }
 }
--- app/src/main/java/com/dgarcia841/tres_en_linea/MainActivity.java
+++ app/src/main/java/com/dgarcia841/tres_en_linea/MainActivity.java
@@ -2,77 +2,115 @@ package com.dgarcia841.tres_en_linea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
-
-import java.net.URI;
-
-import io.socket.client.IO;
-import io.socket.client.Socket;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Componentes de UI con los que interactuar
+    EditText inputUri;
+    EditText inputUser;
+    Button play;
+    Button cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         // Componentes de UI con los que interactuar
-        final EditText inputUri = (EditText) findViewById(R.id.tv_uri);
-        final EditText inputUser = (EditText) findViewById(R.id.tv_username);
-        final Button play = (Button) findViewById(R.id.bt_play);
-        final Button cancel = (Button) findViewById((R.id.bt_cancel));
+        inputUri = (EditText) findViewById(R.id.tv_uri);
+        inputUser = (EditText) findViewById(R.id.tv_username);
+        play = (Button) findViewById(R.id.bt_play);
+        cancel = (Button) findViewById((R.id.bt_cancel));
 
-        // Iniciar conexión al presionar el botón de Play
-        play.setOnClickListener((e) -> {
+        // Configurar eventos
+        play.setOnClickListener(e -> onPlayClicked());
+        cancel.setOnClickListener(e -> onCancelClicked());
 
-            // Obtener texto actual del botón de Play
-            final String text = play.getText().toString();
-            // Obtener datos de conexión (URI del servidor y nombre de usuario)
-            final String serverUri = inputUri.getText().toString();
-            final String username = inputUser.getText().toString();
-
-            // Cambiar el texto de botón Play a Loading
-            play.setText(getString(R.string.button_play_loading));
-            // Marcar como desactivado el botón de play
-            play.setEnabled(false);
-            // Mostrar el botón de cancel
-            cancel.setVisibility(View.VISIBLE);
-            // Cancelar conexión al pulsar el botón de cancel
-            cancel.setOnClickListener((f) -> {
-                GameServer.get().getSocket().disconnect();
-                // Restaurar botones
-                play.setText(text);
-                play.setEnabled(true);
-                cancel.setVisibility(View.INVISIBLE);
+        // Al iniciar la partida
+        GameServer.get().onGameStarted((gameid, rivalname, yourturn) -> {
+            // restaurar los botones
+            this.runOnUiThread(() -> {
+                setButtonsPlay();
             });
 
-            // Conectarse al servidor
-            GameServer.get().onConnect((socket) -> {
-                socket.emit("startGame", username);
-            }).connect(serverUri);
-
-            // Al iniciar la partida
-            GameServer.get().onGameStarted((gameid, rivalname, yourturn) -> {
-                this.runOnUiThread(() -> {
-                    // Restaurar botones
-                    play.setText(text);
-                    play.setEnabled(true);
-                    cancel.setVisibility(View.INVISIBLE);
-                });
-                Log.d("GAME STARTED: ", "gameid: " + gameid + ", rivalname: " + rivalname + ", your turn: " + (yourturn?"yes":"no"));
-            });
+            onGameStarted(gameid, rivalname, yourturn);
         });
+    }
 
-        Log.d("GAME", "App started");
+    /**
+     * Ocurre cuando se encontró un rival y el juego inició
+     * @param gameid ID del juego iniciado
+     * @param rivalname nombre del rival
+     * @param yourturn ¿Es turno del jugador (true) o del rival (false)?
+     */
+    void onGameStarted(String gameid, String rivalname, boolean yourturn) {
+
+        // Crear intent con los datos del juego iniciado
+        Intent intent = new Intent(MainActivity.this, GameActivity.class);
+        Bundle data = new Bundle();
+
+        data.putString("gameid", gameid);
+        data.putString("rivalname", rivalname);
+        data.putBoolean("yourturn", yourturn);
+
+        intent.putExtras(data);
+
+        // Lanzar actividad
+        startActivity(intent);
+    }
+
+    /**
+     * Ocurre cuando se hace click en el botón de cancelar
+     */
+    void onCancelClicked() {
+        // Desconectar
+        GameServer.get().getSocket().disconnect();
+        setButtonsPlay();
+    }
+
+    /**
+     * Ocurre cuando se hace click en el botón de click
+     */
+    void onPlayClicked() {
+        // Obtener datos de conexión (URI del servidor y nombre de usuario)
+        final String serverUri = inputUri.getText().toString();
+        final String username = inputUser.getText().toString();
+
+        setButtonsLoading();
+
+        // Conectarse al servidor
+        GameServer.get().onConnect((socket) -> {
+            socket.emit("startGame", username);
+        }).connect(serverUri);
+    }
+
+
+    /**
+     * Poner los botones en el estado "Play"
+     */
+    void setButtonsPlay() {
+        // Cambiar el texto de botón Play a Play
+        play.setText(getString(R.string.button_play));
+        // Marcar como activado el botón de play
+        play.setEnabled(true);
+        // Ocultar el botón de cancel
+        cancel.setVisibility(View.INVISIBLE);
+    }
+
+    /**
+     * Poner los botones en el estado "Loading"
+     */
+    void setButtonsLoading() {
+        // Cambiar el texto de botón Play a Loading
+        play.setText(getString(R.string.button_play_loading));
+        // Marcar como desactivado el botón de play
+        play.setEnabled(false);
+        // Mostrar el botón de cancel
+        cancel.setVisibility(View.VISIBLE);
     }
 }
