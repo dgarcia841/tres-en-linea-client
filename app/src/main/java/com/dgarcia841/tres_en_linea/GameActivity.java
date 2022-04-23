cdc64883da078bae27bc3e389acb22d497877788
NiuWeb <101196831+dgarcia841@users.noreply.github.com>
2022-04-23 16:44:51 -0500

    configurada conexión con el servidor: URI de conexión y nombre de usuario para entrar a emparejamiento

---
+++ app/src/main/java/com/dgarcia841/tres_en_linea/GameActivity.java
@@ -0,0 +1,14 @@
+package com.dgarcia841.tres_en_linea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
--- app/src/main/java/com/dgarcia841/tres_en_linea/MainActivity.java
+++ app/src/main/java/com/dgarcia841/tres_en_linea/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,14 +25,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView test = (ImageView) findViewById(R.id.iv_test);
-        Glide.with(this).load("http://192.168.0.192:3000").into(test);
 
+        // Componentes de UI con los que interactuar
+        final EditText inputUri = (EditText) findViewById(R.id.tv_uri);
+        final EditText inputUser = (EditText) findViewById(R.id.tv_username);
+        final Button play = (Button) findViewById(R.id.bt_play);
+        final Button cancel = (Button) findViewById((R.id.bt_cancel));
 
-        Socket socket = IO.socket(URI.create("http://192.168.0.192:4500"));
-        socket.on(Socket.EVENT_CONNECT, args -> {
-            Log.d("SOCKET", "connected!");
+        // Iniciar conexión al presionar el botón de Play
+        play.setOnClickListener((e) -> {
+
+            // Obtener texto actual del botón de Play
+            final String text = play.getText().toString();
+            // Obtener datos de conexión (URI del servidor y nombre de usuario)
+            final String serverUri = inputUri.getText().toString();
+            final String username = inputUser.getText().toString();
+
+            // Cambiar el texto de botón Play a Loading
+            play.setText(getString(R.string.button_play_loading));
+            // Marcar como desactivado el botón de play
+            play.setEnabled(false);
+            // Mostrar el botón de cancel
+            cancel.setVisibility(View.VISIBLE);
+            // Cancelar conexión al pulsar el botón de cancel
+            cancel.setOnClickListener((f) -> {
+                GameServer.get().getSocket().disconnect();
+                // Restaurar botones
+                play.setText(text);
+                play.setEnabled(true);
+                cancel.setVisibility(View.INVISIBLE);
+            });
+
+            // Conectarse al servidor
+            GameServer.get().onConnect((socket) -> {
+                socket.emit("startGame", username);
+            }).connect(serverUri);
+
+            // Al iniciar la partida
+            GameServer.get().onGameStarted((gameid, rivalname, yourturn) -> {
+                this.runOnUiThread(() -> {
+                    // Restaurar botones
+                    play.setText(text);
+                    play.setEnabled(true);
+                    cancel.setVisibility(View.INVISIBLE);
+                });
+                Log.d("GAME STARTED: ", "gameid: " + gameid + ", rivalname: " + rivalname + ", your turn: " + (yourturn?"yes":"no"));
+            });
         });
-        socket.connect();
+
+        Log.d("GAME", "App started");
     }
 }
